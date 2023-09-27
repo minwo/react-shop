30ffa5028ba26d36bd83feb090cb48cdbff443eb
minwo <cossn81@gmail.com>
2023-09-28 01:23:17 +0900

    xcbgx
    bxcb

--- src/Main.js
+++ src/Main.js
@@ -1,15 +1,13 @@
 import { useState } from 'react';
 import productInfo from './data.js';
 import { Route, Routes, Link, useNavigate, Outlet } from 'react-router-dom';
-import axios from 'axios';	
+import axios from 'axios';
 
 function Main(){
 	let [shoes, setShoes] = useState(productInfo);
 
 	return(
 		<>
-			<div className="visual"></div>
-			
 			<div className="prd-list">
 				<Cardlist shoes={shoes}/>
 			</div>
@@ -33,7 +31,7 @@ function Cardlist(props){
 		return(
 			<div className="item" key={obj.id}>
 				<Link to={`/detail/${props.shoes[idx].id}`}>
-					<div className="img"><img src={process.env.PUBLIC_URL + "/product/img_prd0"+ (idx+1) +".jpg"} alt="" /></div>
+					<div className="img"><img src={process.env.PUBLIC_URL + "/product/img_prd0"+ (idx+1) +".webp"} alt="" /></div>
 					<p className="title">{obj.title}</p>
 					<p className="detail">{obj.content}</p>
 					<p className="price">{obj.price}</p>
